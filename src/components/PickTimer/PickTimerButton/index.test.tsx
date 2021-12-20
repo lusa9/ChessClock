e9d8aa4439e32a0a87e7ea2b13db17204bbd40bf
Luka Ušalj <luka.usalj@gmail.com>
2021-12-20 16:31:55 +0100

    updated PickTimerButton tests

--- src/components/PickTimer/PickTimerButton/index.test.tsx
+++ src/components/PickTimer/PickTimerButton/index.test.tsx
@@ -1,26 +1,37 @@
-import { render } from "@testing-library/react";
-import Component from ".";
+import { render, fireEvent } from "@testing-library/react";
+import Component, { PickTimerButtonProps } from ".";
+
+const timeMin = 3;
+const incrementSec = 2;
+const onClick = jest.fn();
+
+const setup = (props: PickTimerButtonProps) => render(<Component {...props} />);
 
 test("renders successfully", () => {
-  const { baseElement: element } = render(
-    <Component timeMin={3} incrementSec={2} onClick={jest.fn()} />
-  );
+  const { baseElement: element } = setup({ timeMin, incrementSec, onClick });
 
   expect(element).toBeInTheDocument();
 });
 
 test("renders values in correct format", () => {
-  const { getByText } = render(<Component timeMin={3} onClick={jest.fn()} />);
+  const { getByText } = setup({ timeMin, onClick });
   const element = getByText("3");
 
   expect(element).toBeInTheDocument();
 });
 
 test("renders values with increment in correct format", () => {
-  const { getByText } = render(
-    <Component timeMin={3} incrementSec={2} onClick={jest.fn()} />
-  );
+  const { getByText } = setup({ timeMin, incrementSec, onClick });
   const element = getByText("3 | 2");
 
   expect(element).toBeInTheDocument();
+});
+
+describe("on click", () => {
+  test("triggers callback call", () => {
+    const { container } = setup({ timeMin, incrementSec, onClick });
+    fireEvent.click(container.getElementsByTagName("button")[0]);
+
+    expect(onClick).toBeCalledTimes(1);
+  });
 });
