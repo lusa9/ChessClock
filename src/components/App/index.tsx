f0df4ef9cf641bce096b325e7e7ac7c45361ff33
Luka Ušalj <luka.usalj@gmail.com>
2022-02-01 14:51:28 +0100

    extracted AppClockElement

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,13 +1,12 @@
-import Clock from "components/Clock";
 import Timers from "components/Timers";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-  useParams,
 } from "react-router-dom";
 import Store from "Store";
+import AppClockElement from "./AppClockElement";
 
 export default () => (
   <Store>
@@ -15,31 +14,8 @@ export default () => (
       <Routes>
         <Route path="*" element={<Navigate replace to="/timers" />} />
         <Route path="/timers/*" element={<Timers />} />
-        <Route path="/clock/:params" element={<ClockElement />} />
+        <Route path="/clock/:params" element={<AppClockElement />} />
       </Routes>
     </Router>
   </Store>
 );
-
-const ClockElement = () => {
-  const { params } = useParams();
-  const searchParams = new URLSearchParams(params);
-  const timeString = searchParams.get("time");
-  if (!timeString) {
-    return <Navigate replace to="/timers" />;
-  }
-  const timeMin = parseInt(timeString);
-
-  if (isNaN(timeMin)) {
-    return <Navigate replace to="/timers" />;
-  }
-
-  const incrementString = searchParams.get("increment");
-  const incrementSec = incrementString ? parseInt(incrementString) : undefined;
-
-  if (incrementString && isNaN(parseInt(incrementString))) {
-    return <Navigate replace to="/timers" />;
-  }
-
-  return <Clock {...{ timeMin, incrementSec }} />;
-};
