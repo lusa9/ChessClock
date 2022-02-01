510f40bdb0d80a40779adffb57377ad0683c7dc8
Luka Ušalj <luka.usalj@gmail.com>
2022-02-01 15:29:50 +0100

    formatted AppClockElement

--- src/components/App/AppClockElement/index.tsx
+++ src/components/App/AppClockElement/index.tsx
@@ -1,28 +1,25 @@
 import Clock from "components/Clock";
-import {
-  Navigate,
-  useParams,
-} from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 
 export default () => {
-    const { params } = useParams();
-    const searchParams = new URLSearchParams(params);
-    const timeString = searchParams.get("time");
-    if (!timeString) {
-      return <Navigate replace to="/timers" />;
-    }
-    const timeMin = parseInt(timeString);
-  
-    if (isNaN(timeMin)) {
-      return <Navigate replace to="/timers" />;
-    }
-  
-    const incrementString = searchParams.get("increment");
-    const incrementSec = incrementString ? parseInt(incrementString) : undefined;
-  
-    if (incrementString && isNaN(parseInt(incrementString))) {
-      return <Navigate replace to="/timers" />;
-    }
-  
-    return <Clock {...{ timeMin, incrementSec }} />;
-  };
+  const { params } = useParams();
+  const searchParams = new URLSearchParams(params);
+  const timeString = searchParams.get("time");
+  if (!timeString) {
+    return <Navigate replace to="/timers" />;
+  }
+  const timeMin = parseInt(timeString);
+
+  if (isNaN(timeMin)) {
+    return <Navigate replace to="/timers" />;
+  }
+
+  const incrementString = searchParams.get("increment");
+  const incrementSec = incrementString ? parseInt(incrementString) : undefined;
+
+  if (incrementString && isNaN(parseInt(incrementString))) {
+    return <Navigate replace to="/timers" />;
+  }
+
+  return <Clock {...{ timeMin, incrementSec }} />;
+};
