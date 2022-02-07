3e7d7f9682441b5f9a77d0f515c7e77fabde2e44
Luka Ušalj <luka.usalj@ceracare.co.uk>
2022-02-07 18:27:11 +0100

    added more tests for control buttons

--- src/components/Clock/ControlButtons/index.test.tsx
+++ src/components/Clock/ControlButtons/index.test.tsx
@@ -5,9 +5,16 @@ const onPauseButtonClick = jest.fn();
 const onResetButtonClick = jest.fn();
 const expired = false;
 
-const setup = () =>
+interface SetupProps {
+  expired?: boolean;
+}
+
+const setup = (props?: SetupProps) =>
   render(
-    <Component {...{ onPauseButtonClick, onResetButtonClick, expired }} />
+    <Component
+      {...{ onPauseButtonClick, onResetButtonClick, expired }}
+      {...props}
+    />
   );
 
 it("renders successfully", () => {
@@ -17,7 +24,35 @@ it("renders successfully", () => {
 });
 
 it("renders pause button", () => {
-  const { baseElement: element, container } = setup();
+  const { getByAltText } = setup();
+
+  const element = getByAltText("pause");
 
   expect(element).toBeInTheDocument();
+});
+
+it("renders reset button", () => {
+  const { getByAltText } = setup();
+
+  const element = getByAltText("reset");
+
+  expect(element).toBeInTheDocument();
+});
+
+describe("on expiry", () => {
+  it("doesn't render pause button", () => {
+    const { queryByAltText } = setup({ expired: true });
+
+    const element = queryByAltText("pause");
+
+    expect(element).toBeNull();
+  });
+
+  it("renders reset button", () => {
+    const { getByAltText } = setup({ expired: true });
+
+    const element = getByAltText("reset");
+
+    expect(element).toBeInTheDocument();
+  });
 });
